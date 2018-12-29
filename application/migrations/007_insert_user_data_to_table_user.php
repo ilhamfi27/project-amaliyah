74e78086848c6e1c7881217c8729d7b0c51f626d
Ilham Fadhilah <ilhamfi_2701@yahoo.com>
2018-12-29 08:10:28 +0700

    fix query

--- application/migrations/007_insert_user_data_to_table_user.php
+++ application/migrations/007_insert_user_data_to_table_user.php
@@ -1,10 +1,10 @@
 <?php
 class Migration_insert_user_data_to_table_user extends CI_Migration{
     public function up(){
-        $this->db->query("INSERT INTO `users`(`id`,`username`, `password`, `admin`) VALUES (1,'admin','21232f297a57a5a743894a0e4a801fc3','t')");
+        $this->db->query("INSERT INTO users(id,username, password, admin) VALUES (1,'admin','21232f297a57a5a743894a0e4a801fc3','t')");
     }
 
     public function down(){
-        $this->db->query("DELETE FROM `users` WHERE `id`=1");
+        $this->db->query("DELETE FROM users WHERE id=1");
     }
 }
