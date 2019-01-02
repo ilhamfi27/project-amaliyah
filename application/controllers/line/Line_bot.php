cbdc94d37822ba5ca90b03cbff742bd9e67ea872
Ilham Fadhilah <ilhamfi_2701@yahoo.com>
2019-01-02 18:05:22 +0700

    testing first commit line bot webhook

--- application/controllers/line/Line_bot.php
+++ application/controllers/line/Line_bot.php
@@ -9,6 +9,20 @@ class Line_bot extends CI_Controller{
     }
 
     public function webhook(){
-
+        // init bot
+        $httpClient = new \LINE\LINEBot\HTTPClient\CurlHTTPClient($_ENV['CHANNEL_ACCESS_TOKEN']);
+        $bot = new \LINE\LINEBot($httpClient, ['channelSecret' => $_ENV['CHANNEL_SECRET']]);
+        $data = json_decode($body, true);
+        foreach ($data['events'] as $event)
+        {
+            $userMessage = $event['message']['text'];
+            if(strtolower($userMessage) == 'halo')
+            {
+                $message = "Halo juga";
+                $textMessageBuilder = new \LINE\LINEBot\MessageBuilder\TextMessageBuilder($message);
+                $result = $bot->replyMessage($event['replyToken'], $textMessageBuilder);
+                return $result->getHTTPStatus() . ' ' . $result->getRawBody();
+            }
+        }
     }
 }
