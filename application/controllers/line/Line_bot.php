7a7bdcce002ab796073a5dc54aae45914e803988
Ilham Fadhilah <ilhamfi_2701@yahoo.com>
2019-01-02 20:56:13 +0700

    line bot development 1

--- application/controllers/line/Line_bot.php
+++ application/controllers/line/Line_bot.php
@@ -1,28 +1,56 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
+require 'vendor/autoload.php';
 
-use \LINE\LINEBot\SignatureValidator as SignatureValidator;
+use LINE\LINEBot\Constant\HTTPHeader;
+use LINE\LINEBot\Event\MessageEvent;
+use LINE\LINEBot\Event\MessageEvent\TextMessage;
 
 class Line_bot extends CI_Controller{
-    public function index(){
-        echo "hai";
+    private $app;
+    public function __construct() {
+        $dotenv = new Dotenv\Dotenv(__DIR__);
+        $dotenv->load();
     }
 
-    public function webhook(){
-        // init bot
-        $httpClient = new \LINE\LINEBot\HTTPClient\CurlHTTPClient($_ENV['CHANNEL_ACCESS_TOKEN']);
-        $bot = new \LINE\LINEBot($httpClient, ['channelSecret' => $_ENV['CHANNEL_SECRET']]);
-        $data = json_decode($body, true);
-        foreach ($data['events'] as $event)
-        {
-            $userMessage = $event['message']['text'];
-            if(strtolower($userMessage) == 'halo')
-            {
-                $message = "Halo juga";
-                $textMessageBuilder = new \LINE\LINEBot\MessageBuilder\TextMessageBuilder($message);
-                $result = $bot->replyMessage($event['replyToken'], $textMessageBuilder);
-                return $result->getHTTPStatus() . ' ' . $result->getRawBody();
+    public function index(){
+        echo "qwe";
+    }
+
+    public function webhook(\Slim\App $app){
+        $app->post('/callback', function (\Slim\Http\Request $req, \Slim\Http\Response $res) {
+            /** @var \LINE\LINEBot $bot */
+            $bot = $this->bot;
+            /** @var \Monolog\Logger $logger */
+            $logger = $this->logger;
+            $signature = $req->getHeader(HTTPHeader::LINE_SIGNATURE);
+            if (empty($signature)) {
+                return $res->withStatus(400, 'Bad Request');
             }
-        }
+            // Check request with signature and parse request
+            try {
+                $events = $bot->parseEventRequest($req->getBody(), $signature[0]);
+            } catch (InvalidSignatureException $e) {
+                return $res->withStatus(400, 'Invalid signature');
+            } catch (InvalidEventRequestException $e) {
+                return $res->withStatus(400, "Invalid event request");
+            }
+            foreach ($events as $event) {
+                if (!($event instanceof MessageEvent)) {
+                    $logger->info('Non message event has come');
+                    continue;
+                }
+                if (!($event instanceof TextMessage)) {
+                    $logger->info('Non text message has come');
+                    continue;
+                }
+                $replyText = $event->getText();
+                $logger->info('Reply text: ' . $replyText);
+                $resp = $bot->replyText($event->getReplyToken(), $replyText);
+                $logger->info($resp->getHTTPStatus() . ': ' . $resp->getRawBody());
+            }
+            $res->write('OK');
+            return $res;
+        });
     }
 }
