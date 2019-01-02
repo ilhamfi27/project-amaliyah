d87c2a0ff75a2f3997980f9fcbc1fcd5f36f2799
Ilham Fadhilah <ilhamfi_2701@yahoo.com>
2019-01-02 17:41:36 +0700

    little changs

--- application/config/config.php
+++ application/config/config.php
@@ -23,7 +23,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'http://localhost/amaliyah-app';
+$config['base_url'] = $_ENV['CI_ENV'] === "staging" ? 'https://amaliyah-app-staging.herokuapp.com/' : 'http://localhost/amaliyah-app';
 
 /*
 |--------------------------------------------------------------------------
@@ -380,7 +380,7 @@ $config['encryption_key'] = '';
 $config['sess_driver'] = 'files';
 $config['sess_cookie_name'] = 'ci_session';
 $config['sess_expiration'] = 7200;
-$config['sess_save_path'] = 'ci_session';
+$config['sess_save_path'] = NULL;
 $config['sess_match_ip'] = FALSE;
 $config['sess_time_to_update'] = 300;
 $config['sess_regenerate_destroy'] = FALSE;
---
+++ application/controllers/line/Line_bot.php
@@ -0,0 +1,14 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+use \LINE\LINEBot\SignatureValidator as SignatureValidator;
+
+class Line_bot extends CI_Controller{
+    public function index(){
+        echo "hai";
+    }
+
+    public function webhook(){
+
+    }
+}
