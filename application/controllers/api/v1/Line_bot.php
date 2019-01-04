1d3eacd66cbc5faf9931b41ce1cf801057972ecc
Ilham Fadhilah <ilhamfi_2701@yahoo.com>
2019-01-04 13:25:59 +0700

    configure bot api file

--- application/controllers/api/v1/Line_bot.php
+++ application/controllers/api/v1/Line_bot.php
@@ -2,6 +2,9 @@
 defined('BASEPATH') OR exit('No direct script access allowed');
 require 'vendor/autoload.php';
 
+use LINE\LINEBot\SignatureValidator as SignatureValidator;
+use LINE\LINEBot;
+use LINE\LINEBot\HTTPClient\CurlHTTPClient;
 use LINE\LINEBot\Constant\HTTPHeader;
 use LINE\LINEBot\Event\MessageEvent;
 use LINE\LINEBot\Event\MessageEvent\TextMessage;
@@ -10,11 +13,19 @@ use LINE\LINEBot\Exception\InvalidSignatureException;
 
 class Line_bot extends CI_Controller{
     private $app;
+    private $channel_access_token;
+    private $channel_secret;
+    private $pass_signature;
+    private $httpClient;
+    private $bot;
+
+
     public function __construct() {
         if($this->environment_is("development")){
             $this->dotenv_initiation();
         }
         $this->slim_config();
+        $this->line_bot_initiation();
         $this->slim_api();
     }
 
@@ -28,6 +39,19 @@ class Line_bot extends CI_Controller{
         $this->app = new Slim\App($configs);
     }
 
+    private function line_bot_initiation(){
+        // set false for production
+        $this->pass_signature = true;
+        
+        // set LINE channel_access_token and channel_secret
+        $this->channel_access_token = isset($_ENV['CHANNEL_ACCESS_TOKEN']) ? $_ENV['CHANNEL_ACCESS_TOKEN'] : "";
+        $this->channel_secret = isset($_ENV['CHANNEL_SECRET']) ? $_ENV['CHANNEL_SECRET'] : "";
+        
+        // bot object initiation
+        $this->httpClient = new CurlHTTPClient($this->channel_access_token);
+        $this->bot = new LINEBot($this->httpClient, ['channelSecret' => $this->channel_secret]);
+    }
+
     private function slim_api(){
         $this->app->get('/api_test', function () {
             return "hello, it works!";
@@ -35,37 +59,24 @@ class Line_bot extends CI_Controller{
         $this->app->get('/api/line/webhook', function ($request, $response) {
             return $response->withStatus(200);
         });
-        $this->app->post('/api/line/webhook', function ($request, $response)
-        {
+        $this->app->post('/api/line/webhook', function ($request, $response){
+            
             // get request body and line signature header
-            $body 	   = file_get_contents('php://input');
-            $signature = $_SERVER['HTTP_X_LINE_SIGNATURE'];
+            $body        = file_get_contents('php://input');
+            $signature = isset($_SERVER['HTTP_X_LINE_SIGNATURE']) ? $_SERVER['HTTP_X_LINE_SIGNATURE'] : '';
+            
             // log body and signature
             file_put_contents('php://stderr', 'Body: '.$body);
+
             // is LINE_SIGNATURE exists in request header?
             if (empty($signature)){
                 return $response->withStatus(400, 'Signature not set');
             }
+
             // is this request comes from LINE?
             if($_ENV['PASS_SIGNATURE'] == false && ! SignatureValidator::validateSignature($body, $_ENV['CHANNEL_SECRET'], $signature)){
                 return $response->withStatus(400, 'Invalid signature');
             }
-            // init bot
-            $httpClient = new \LINE\LINEBot\HTTPClient\CurlHTTPClient($_ENV['CHANNEL_ACCESS_TOKEN']);
-            $bot = new \LINE\LINEBot($httpClient, ['channelSecret' => $_ENV['CHANNEL_SECRET']]);
-            $data = json_decode($body, true);
-            foreach ($data['events'] as $event)
-            {
-                $userMessage = $event['message']['text'];
-                if(strtolower($userMessage) == 'halo')
-                {
-                    $message = "Halo juga";
-                    $textMessageBuilder = new \LINE\LINEBot\MessageBuilder\TextMessageBuilder($message);
-                    $result = $bot->replyMessage($event['replyToken'], $textMessageBuilder);
-                    return $result->getHTTPStatus() . ' ' . $result->getRawBody();
-                }
-            }
-            
         });
         $this->app->run();
     }
