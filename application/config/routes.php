29a3cafa42de29775f4c26d0484c410e71fb9d58
Ilham Fadhilah <ilhamfi_2701@yahoo.com>
2019-01-08 20:48:04 +0700

    change route on API

--- application/config/routes.php
+++ application/config/routes.php
@@ -52,5 +52,3 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 $route['default_controller'] = 'user_auth';
 $route['404_override'] = '';
 $route['translate_uri_dashes'] = FALSE;
-$route['api/line'] = 'api/v1/Line_bot';
-$route['api/line/webhook'] = 'api/v1/Line_bot/webhook';
